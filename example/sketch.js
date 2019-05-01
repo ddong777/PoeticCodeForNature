769ad997dfedbe9e08144cb61442f2dc901782ac
ddong777 <dj060352@naver.com>
2019-05-01 12:12:36 +0900

    change text pos

--- example/sketch.js
+++ example/sketch.js
@@ -1,8 +1,9 @@
 let canvas;
+let x, y;
 
 function centerCanvas() {
-  let x = (windowWidth - width) / 2;
-  let y = (windowHeight - height) / 2;
+  x = (windowWidth - width) / 2;
+  y = (windowHeight - height) / 2;
   canvas.position(x, y);
 }
 
@@ -18,8 +19,9 @@ function setup() {
   작품에 대한 설명이 들어갑니다. <br/> \
   HTML이 직접 들어가서 줄넘김을 할 수 있습니다. \
   ";
+
   text = createDiv(description);
-  text.position(canvas.width + 20, canvas.height + 20);
+  text.position(x + 20, y);
   text.style("font-family", "monospace");
   text.style("font-size", "12pt");
 
