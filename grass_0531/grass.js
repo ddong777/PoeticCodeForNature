09322fa26316af0b765ef7c5f1a15a99202a1ec9
ddong777 <dj060352@naver.com>
2019-06-19 05:14:49 +0900

    add moving
    and make it get energy from light

--- grass_0531/grass.js
+++ grass_0531/grass.js
@@ -2,7 +2,7 @@ class grass {
   constructor(x, y) {
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
-    this.acc = createVector(random(0.01, 0.1), random(0.01, 0.1));
+    this.acc = createVector(0, 0);
 
     this.density = 100;
 
@@ -15,21 +15,31 @@ class grass {
     this.startR = random(5, 10);
     this.middleR = random(30, 40);
     this.endR = 0;
+  }
 
+  addForce(force){
+    this.acc.add(force);
   }
 
   update() {
-    this.acc = p5.Vector.random2D();
-    this.acc.setMag(0.05);
+    this.addForce(p5.Vector.random2D());
+
+    this.acc.setMag(0.1);
+
     this.vel.add(this.acc);
     this.pos.add(this.vel);
-    this.acc.set(0, 0);
 
     this.growTime -= 0.1;
     this.r += this.growUP;
 
     this.growPhase1();
     this.lifespan();
+  }
+
+  followLight(attractor){
+    let followingForce = p5.Vector.sub(attractor, this.pos);
+    followingForce.setMag(0.3);
+    this.addForce(followingForce);
   }
 
   display() {
@@ -48,6 +58,9 @@ class grass {
 
   lifespan(){
     this.lifeTime --;
+    if (lightOn){
+      this.lifeTime += 0.5;
+    }
     if (this.lifeTime <= 0){
       this.die = true;
     } else {
