f80ec11b231fc74437c5e043acb6e3d6833968fe
ddong777 <dj060352@naver.com>
2019-06-23 00:46:05 +0900

    remove light seeking

--- grass_0622/branch.js
+++ grass_0622/branch.js
@@ -4,19 +4,8 @@ class branch {
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
 
-    this.desired = createVector(0, 0);
-    this.steering = createVector(0, 0);
-    this.d = createVector(0, 0);
-
-    this.maxSpeed = 2;
-    this.maxForce = 0.5;
-
-    this.density = 100;
-    this.r = 10;
-
-    this.energy = 50;
-    this.growSpeed = createVector(0, 0);
-    this.growWeight = 0;
+    this.energy = random(50, 70);
+    this.makeDivergence = false;
   }
 
   applyForce(force){
@@ -24,50 +13,34 @@ class branch {
   }
 
   update() {
-    this.applyForce(p5.Vector.random2D(1));
-
+    this.applyForce(p5.Vector.random2D());
+    this.acc.setMag(0.07);
     this.vel.add(this.acc);
-    this.steering.limit(this.maxForce);
-    this.vel.limit(this.maxForce);
-
     this.pos.add(this.vel);
   }
 
-  seekLight(target){
-    this.desired = p5.Vector.sub(target, this.pos);
-
-    this.d = this.desired.mag();
-    if (this.d < 80){
-      this.m = map(this.d, 0, 100, 0, this.maxSpeed);
-      this.desired.setMag(this.m);
-    } else if (this.d > 200){
-      this.applyForce(p5.Vector.random2D(1));
-    } else {
-      this.desired.setMag(this.maxSpeed);
-    }
-
-    this.steering = p5.Vector.sub(this.desired, this.vel);
-    this.applyForce(this.steering);
-  }
-
   growing(){
-    if (this.d < 200){
-      this.energy += random(-0.7, 0.7);
-    } else {
-      this.energy -= 0.1;
+    if (this.energy <= 70 && this.energy > 5){
+      this.energy += random(-0.9, 0.5);
+    } else if (this.energy > 70){
+      this.energy -= 5;
+    } else if (this.energy >= -70) {
+      this.energy += random(-0.5, 0.9);
+    } else if (this.energy < -70){
+      this.energy += 5;
     }
 
-    if (this.energy <= 0.05){
-      this.energy = 0;
-      strokeWeight(0);
+    if (this.energy < 3.1 && this.energy > 3 ){
+      this.makeDivergence = true;
+    } else {
+      this.makeDivergence = false;
     }
   }
 
   display() {
     this.growing();
-
-    stroke(0, this.density);
-    fill(255, this.density);
+    stroke(50, 100);
+    fill(255, 100);
     ellipse(this.pos.x, this.pos.y, this.energy);
   }
 }
