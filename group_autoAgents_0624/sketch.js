74b6633ca0df16edbc5e1e192c5b92884ce8cfaf
ddong777 <dj060352@naver.com>
2019-06-26 05:49:51 +0900

    fix title

--- group_autoAgents_0624/sketch.js
+++ group_autoAgents_0624/sketch.js
@@ -46,7 +46,7 @@ function draw() {
   // print(head.pos.x);
   push();
   fill(255);
-  title = createElement('h2', "</a> 2.multicellular");
+  title = createElement('h2', "</a> 2.Cohesion Before Explosion");
   title.position(windowWidth/2 - 80, 0);
   pop();
 }
